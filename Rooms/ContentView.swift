606654ad8728264bafea28010c62622f9c395874
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-04-20 12:39:21 -0700

    first commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Sebastian Abarca on 4/20/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Image(systemName: "photo")
+            
+            VStack(alignment: .leading) {
+                Text("Rooms")
+                Text("20 people")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
